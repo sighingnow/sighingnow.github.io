05aa5c36071cd76ac0b18d9d9254e81c1eb6e119
He Tao <hetao@buaa.edu.cn>
2015-06-05 19:14:11 +0800

    add a new post about Polya.

--- build.py
+++ build.py
@@ -3,16 +3,20 @@
 
 import os
 
+
 def build_pages(src, dst):
     for f in os.listdir(src):
-        srcpath, dstpath = '%s/%s'%(src, f), '%s/%s'%(dst, f)
+        srcpath, dstpath = '%s/%s' % (src, f), '%s/%s' % (dst, f)
         if os.path.isdir(srcpath):
             build_pages(srcpath, dstpath)
         elif f.endswith('.md'):
             if not os.path.exists(dst):
                 os.makedirs(dst)
-            print('build %s'%(srcpath))
-            os.system('pandoc %s -o %s --mathjax --highlight-style=pygments -S'%(srcpath, dstpath[:-3]+'.html'))
+            print('build %s' % srcpath)
+            os.system(
+                'pandoc %s -o %s --mathjax --highlight-style=pygments -S'
+                % (srcpath, dstpath[:-3]+'.html')
+            )
             meta, text = '', ''
             with open(srcpath, 'r', encoding='utf-8') as fp:
                 meta = '\n'.join(fp.read().split('\n')[0:9])+'\n'
